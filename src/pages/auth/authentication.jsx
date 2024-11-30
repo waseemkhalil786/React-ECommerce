ead9a6f1596379adc555e9357ebb84f62706ca50
Waseem Khalil <waseemkhalilworld@gmail.com>
2024-11-30 03:16:02 -0800

    "working on login signup"

---
+++ src/pages/auth/authentication.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdE8tw5Dx1_tNbBhOQ2cY-HEQENEf2gFk===",
+  authDomain: "authentication-593aa.firebaseapp.com",
+  projectId: "authentication-593aa",
+  storageBucket: "authentication-593aa.firebasestorage.app",
+  messagingSenderId: "880824069493",
+  appId: "1:880824069493:web:0d0534a174093d4e64f73c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
